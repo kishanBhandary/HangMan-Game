029bae5778020f27179c279b33a636a833b7588f
kishanBhandary <kishanbhandary0@gmail.com>
2025-08-05 18:34:21 +0530

    deleete code

--- src/HangmanGUI.java
+++ src/HangmanGUI.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class HangmanGUI extends JFrame {
     private static final String[] WORDS = {
             "PROGRAMMING", "COMPUTER", "JAVA", "HANGMAN", "CHALLENGE",
-            "DEVELOPER", "SOFTWARE", "ALGORITHM", "DATABASE", "NETWORK","C++","JAVA"};
+            "DEVELOPER", "SOFTWARE", "ALGORITHM", "DATABASE", "NETWORK","C++"};
 
     private String wordToGuess;
     private char[] guessedWord;
