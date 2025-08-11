8c7f2788853566a9f53fabb08991cef1302c76e1
kishanBhandary <kishanbhandary0@gmail.com>
2025-08-11 17:56:14 +0530

    Add code

--- src/HangmanGUI.java
+++ src/HangmanGUI.java
@@ -32,7 +32,7 @@ public class HangmanGUI extends JFrame {
     }
 
     private void initializeGUI() {
-        setTitle("");
+        setTitle("Hangman Game using Swing");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout(10, 10));
         setResizable(false);
