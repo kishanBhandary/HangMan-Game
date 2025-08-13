9cc64efa77ce3dd96511d203563a7c959a5766d3
kishanBhandary <kishanbhandary0@gmail.com>
2025-08-13 17:42:27 +0530

    Add code

--- src/HangmanGUI.java
+++ src/HangmanGUI.java
@@ -32,7 +32,7 @@ public class HangmanGUI extends JFrame {
     }
 
     private void initializeGUI() {
-        setTitle("");
+        setTitle("sss");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout(10, 10));
         setResizable(false);
