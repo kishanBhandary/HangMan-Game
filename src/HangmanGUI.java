c968cd358867ae705bebafc96aaa9d8bc1d6a3cd
kishanBhandary <kishanbhandary0@gmail.com>
2025-08-20 19:49:56 +0530

    Add code

--- src/HangmanGUI.java
+++ src/HangmanGUI.java
@@ -32,7 +32,7 @@ public class HangmanGUI extends JFrame {
     }
 
     private void initializeGUI() {
-        setTitle(" ");
+        setTitle("Hangman Game");
         setDefaultLookAndFeelDecorated(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout(10, 10));
