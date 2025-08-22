3bea64ef40949da1eca2e5b3d0517e40fb4eb620
kishanBhandary <kishanbhandary0@gmail.com>
2025-08-22 19:21:27 +0530

    Add code

--- src/HangmanGUI.java
+++ src/HangmanGUI.java
@@ -231,7 +231,7 @@ public class HangmanGUI extends JFrame {
             statusLabel.setText("💀 GAME OVER! The word was: " + wordToGuess);
             statusLabel.setForeground(Color.RED);
             disableInput();
-            showGameEndDialog("Game Oveeeeeee!", "You lost! The word was: " + wordToGuess);
+            showGameEndDialog("Game Oveeeeee!", "You lost! The word was: " + wordToGuess);
         }
     }
 
