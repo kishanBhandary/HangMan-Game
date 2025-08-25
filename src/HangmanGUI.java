68f65bde40a71105099597d1402b3344dd4a30be
kishanBhandary <kishanbhandary0@gmail.com>
2025-08-25 19:48:39 +0530

    Add code

--- src/HangmanGUI.java
+++ src/HangmanGUI.java
@@ -213,7 +213,7 @@ public class HangmanGUI extends JFrame {
             }
             guessedLettersLabel.setText(guessed.toString());
         } else {
-            guessedLettersLabel.setText("Gueeeee: ");
+            guessedLettersLabel.setText("Gueeeeee: ");
         }
 
         // Update hangman drawing
